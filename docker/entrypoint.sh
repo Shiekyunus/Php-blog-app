0552c3c7c130020a1b14a7861d7262dbd1d2855b
Shiek Yunus <shiekyunus2005@gmail.com>
2026-06-11 07:16:34 +0000

    Add db:seed to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 php /var/www/html/artisan migrate --force
+php /var/www/html/artisan db:seed --force
 php /var/www/html/artisan config:clear
 php /var/www/html/artisan cache:clear
 exec "$@"
