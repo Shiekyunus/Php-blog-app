afc73f7b1d2346b7995a21e3b4f059c621c2ac5c
Shiek Yunus <shiekyunus2005@gmail.com>
2026-06-11 09:13:32 +0000

    Force Https scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,20 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\View;
-use App\View\Composers\AdminLayoutComposer;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    // Register the AdminLayoutComposer to be used with the 'layouts.admin' view, allowing it to inject necessary data into that view.
     public function boot(): void
     {
-        //
-        View::composer('layouts.admin', AdminLayoutComposer::class);
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
