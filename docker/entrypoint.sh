54c5b4bfa8f61cd24444b6179e0fd7b6443d58b8
Shiek Yunus <shiekyunus2005@gmail.com>
2026-06-11 08:26:59 +0000

    Fix seeder to use RoleAndPermissionSeeder only

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 php /var/www/html/artisan migrate --force
-php /var/www/html/artisan db:seed --force
+php /var/www/html/artisan db:seed --class=RoleAndPermissionSeeder --force
 php /var/www/html/artisan config:clear
-php /var/www/html/artisan cache:clear
 exec "$@"
