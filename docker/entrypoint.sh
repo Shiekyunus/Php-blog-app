3f3af96d9e09450af0adec81e90036be0a31b2ba
Shiekyunus <shiekyunus2005@gmail.com>
2026-06-26 10:12:33 +0530

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+set -e
+
 php /var/www/html/artisan migrate --force
 php /var/www/html/artisan db:seed --class=RoleAndPermissionSeeder --force
 php /var/www/html/artisan config:clear
-exec "$@"
+
+# Start php-fpm in background
+php-fpm -D
+
+# Start nginx in foreground (keeps container alive)
+exec nginx -g 'daemon off;'
